495c152fad5340cc99067cf717d42fdd19256386
TMAdevby <mtipun@mail.ru>
2025-10-28 19:17:20 +0300

    http3

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,6 +1,19 @@
 package ru.netology;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpHeaders;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -10,6 +23,37 @@ public class Main {
     public static ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) {
+        ConnectionConfig connConfig = ConnectionConfig.custom()
+                .setConnectTimeout(5000, TimeUnit.SECONDS)
+                .setSocketTimeout(5000,TimeUnit.MILLISECONDS)
+                .build();
 
+        BasicHttpClientConnectionManager cm = new BasicHttpClientConnectionManager();
+        cm.setConnectionConfig(connConfig);
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My Test Service")
+                .setConnectionManager(cm)
+                .build();
+// создание объекта запроса с произвольными заголовками
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+// отправка запроса
+        CloseableHttpResponse response = httpClient.execute(request);
+// вывод полученных заголовков
+        Arrays.stream(response.getHeaders()).forEach(System.out::println);
+// чтение тела ответа
+//        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+//        System.out.println(body);
+
+        List<Post> posts = mapper.readValue(
+                response.getEntity().getContent(),
+                new TypeReference<List<Post>>() {
+                }
+        );
+        posts.forEach(System.out::println);
+
+        response.close();
+        httpClient.close()
     }
 }
